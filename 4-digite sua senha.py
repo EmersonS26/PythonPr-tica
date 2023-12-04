738b53784c85897a14fa56d926512a3d956a500c
Emerson S <132622930+secthor01@users.noreply.github.com>
2023-12-04 13:13:26 -0300

    Autorização de senha
    Projeto iniciante onde peço para que o usuário digite a senha e se a senha não for correta, ele irá pedir que tente novamente até a senha estar correta.

---
+++ 4-digite sua senha.py
@@ -0,0 +1,6 @@
+senha = input("Digite sua senha para autorização do acesso: ")
+
+while(senha != "Dorminapraça"):
+    senha = input("Senha inválida, tente novamente: ")
+    
+print("Senha correta! Acesso autorizado...")
