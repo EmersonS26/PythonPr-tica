610a21a5cca030756bc82241f4ca70962a952bea
Emerson S <132622930+secthor01@users.noreply.github.com>
2023-12-06 18:04:29 -0300

    Update 1-EquipeVencedora.py

---
+++ 1-EquipeVencedora.py
@@ -0,0 +1,9 @@
+Equipe_A = input("Digite seu número: (A) ")
+Equipe_B = input("Digite seu número: (B) ")
+
+if(Equipe_A > Equipe_B):
+    print("Equipe A venceu!")
+elif(Equipe_B > Equipe_A):
+    print("Equipe B venceu!")
+else:
+    print("Empate entre as equipes!")    
